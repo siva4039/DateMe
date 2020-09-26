3dd66a2969cc937d1b50af0218d3e6b5a0aea316
sivakumar <52204776+siva4039@users.noreply.github.com>
2020-09-26 16:40:14 +0530

    Initial Commit

---
+++ DateMe.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DateMe.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DateMe.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+        
+        public DbSet<Value> values { get; set; }
+        
+    }
+}
